54f12b84d40ae4e490a4464adee8a5579d22a347
Lincoln Stein <33900458+ltstein@users.noreply.github.com>
2021-01-26 09:12:31 -0600

    WIP: Increase max frame rate and size of grid

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 #include "renderer.h"
 
 int main() {
-  constexpr std::size_t kFramesPerSecond{60};
+  constexpr std::size_t kFramesPerSecond{120};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
   constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{32};
-  constexpr std::size_t kGridHeight{32};
+  constexpr std::size_t kGridWidth{64};
+  constexpr std::size_t kGridHeight{64};
 
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
