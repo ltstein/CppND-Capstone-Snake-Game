fe5871fb22cb5a6e0009f0df94d8932bcdcb5093
Lincoln Stein <33900458+ltstein@users.noreply.github.com>
2021-01-26 06:55:06 -0600

    WIP: Interim testing changes for automatic control

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,11 +1,14 @@
 #include "controller.h"
-#include <iostream>
 #include "SDL.h"
 #include "snake.h"
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
                                  Snake::Direction opposite) const {
-  if (snake.direction != opposite || snake.size == 1) snake.direction = input;
+  if (snake.direction != opposite || snake.size == 1)
+    snake.direction = input;
   return;
 }
 
@@ -16,26 +19,45 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
       running = false;
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
-        case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
-          break;
+      case SDLK_UP:
+        ChangeDirection(snake, Snake::Direction::kUp, Snake::Direction::kDown);
+        break;
 
-        case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
-          break;
+      case SDLK_DOWN:
+        ChangeDirection(snake, Snake::Direction::kDown, Snake::Direction::kUp);
+        break;
 
-        case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
-          break;
+      case SDLK_LEFT:
+        ChangeDirection(snake, Snake::Direction::kLeft,
+                        Snake::Direction::kRight);
+        break;
 
-        case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
-          break;
+      case SDLK_RIGHT:
+        ChangeDirection(snake, Snake::Direction::kRight,
+                        Snake::Direction::kLeft);
+        break;
       }
     }
+  }
+}
+
+void Controller::navigate(Snake &snake, int move) const {
+  Snake::Direction dir = (Snake::Direction)move;
+  switch (dir) {
+  case Snake::Direction::kUp:
+    ChangeDirection(snake, Snake::Direction::kUp, Snake::Direction::kDown);
+    break;
+
+  case Snake::Direction::kDown:
+    ChangeDirection(snake, Snake::Direction::kDown, Snake::Direction::kUp);
+    break;
+
+  case Snake::Direction::kLeft:
+    ChangeDirection(snake, Snake::Direction::kLeft, Snake::Direction::kRight);
+    break;
+
+  case Snake::Direction::kRight:
+    ChangeDirection(snake, Snake::Direction::kRight, Snake::Direction::kLeft);
+    break;
   }
 }
--- src/controller.h
+++ src/controller.h
@@ -6,10 +6,17 @@
 class Controller {
  public:
   void HandleInput(bool &running, Snake &snake) const;
+  void navigate(Snake &snake, int move) const;
 
  private:
   void ChangeDirection(Snake &snake, Snake::Direction input,
                        Snake::Direction opposite) const;
+
+//   Snake::Direction[] calculatePath(SDL_Point goalLocation, SDL_Point startLocation);
+
+  
+
+
 };
 
 #endif
